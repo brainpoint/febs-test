84772217a065ea66bfbf8eb0e79ae2fa8d90ba78
tian <hutianzhi@tjitech.cn>
2017-11-17 12:17:35 +0800

    [feature]带服务器控制的测试流程

--- test.js
+++ test.js
@@ -6,7 +6,7 @@
 * Desc: 
 */
 
-var test = require('./lib');
+var test = require('./lib/sync_index');
 var http = require('http');
 
 //
@@ -15,7 +15,7 @@ function test_work(pid, tid){ // or async function test_work(pid, tid) {
     // todo.
     test.add_count('request', 1);
     let token = test.begin('request nodjes');
-    http.get('http://nodejs.org/dist/index.json', (res) => {
+    http.get('http://www.baidu.com', (res) => {
       const { statusCode } = res;
       let error;
       if (statusCode !== 200) {
@@ -27,15 +27,17 @@ function test_work(pid, tid){ // or async function test_work(pid, tid) {
       test.finish();
     }).on('error', (e) => {
       test.end_failure(token);
+      test.finish();
     });
 }
 
 //
 // begin test.
 test.start({
-            clientTotal: 100,          // 客户端总数.
-            processNum:  10,           // 进程数. 默认50个.
-            createDurtion: 5000,        // in ms, 模拟客户端在此时间段内创建完成. 默认10000
-            testDurtion : 20000,          // in ms, 测试的持续时间.
-            debug: false
-          }, test_work);
+  host: "http://192.168.0.200:18082",
+  clientTotal: 10,           // 客户端总数.
+  processNum: 5,            // 进程数. 默认50个.
+  createDurtion: 10000,        // in ms, 模拟客户端在此时间段内创建完成. 默认10000
+  testDurtion: 20000,          // in ms, 测试的持续时间.
+  debug: false
+}, test_work);
